df6af3631873413edde151221125142be3dd3113
i92run <48200564+i92run@users.noreply.github.com>
2024-01-19 11:13:22 +0900

    Update RubiksCube777.py

--- rubikscubennnsolver/RubiksCube777.py
+++ rubikscubennnsolver/RubiksCube777.py
@@ -3167,6 +3167,29 @@ class RubiksCube777(RubiksCubeNNNOddEdges):
         tmp_solution_len = len(self.solution)
         self.lt_LR_oblique_edge_pairing.solve_via_c(use_kociemba_string=True)
         self.print_cube_add_comment("LR oblique edges staged", tmp_solution_len)
+    
+        # phase 3 - use 5x5x5 solver to stage the LR centers
+        tmp_solution_len = len(self.solution)
+        self.create_fake_555_from_outside_centers()
+
+        if all_centers:
+            self.fake_555.group_centers_stage_LR()
+            desc = "LR centers staged"
+        else:
+            self.fake_555.lt_LR_t_centers_stage_ida.solve_via_c()
+            desc = "LR t-centers staged"
+
+        for step in self.fake_555.solution:
+            if step.startswith("COMMENT"):
+                pass
+            else:
+                if step.startswith("5"):
+                    step = "7" + step[1:]
+                elif step.startswith("3"):
+                    raise Exception("5x5x5 solution has 3 wide turn")
+                self.rotate(step)
+        
+        self.print_cube_add_comment(desc, tmp_solution_len)
         
     def stage_LR_centers(self):
         self._stage_LR_centers_common(True)
@@ -3263,11 +3286,6 @@ class RubiksCube777(RubiksCubeNNNOddEdges):
             return
 
         if False:
-            # phase 4 - stage UD inner centers, pair UD oblique edges
-
-            # This works but can take 70s so this is disabled until I can figure out a better heuristic so that
-            # it will run faster.  It takes ~15 moves though which is a nice ~9 moves savings over doing phases 4
-            # and 5 invididually.
             tmp_solution_len = len(self.solution)
             self.lt_phase4.solve_via_c(use_kociemba_string=True)
             self.print_cube_add_comment("UD inner x-centers staged, oblique edges paired", tmp_solution_len)
@@ -3278,6 +3296,29 @@ class RubiksCube777(RubiksCubeNNNOddEdges):
             self.lt_UD_oblique_edge_pairing.solve_via_c(use_kociemba_string=True)
             # $ self.lt_UD_oblique_edge_pairing_new.solve_via_c()
             self.print_cube_add_comment("UD oblique edges paired", tmp_solution_len)
+    
+        # phase 6 - use 5x5x5 to stage the UD centers
+        tmp_solution_len = len(self.solution)
+        self.create_fake_555_from_outside_centers()
+
+        if all_centers:
+            self.fake_555.group_centers_stage_FB()
+            desc = "UD centers staged"
+        else:
+            self.fake_555.lt_UD_t_centers_stage_ida.solve_via_c()
+            desc = "UD t-centers staged"
+
+        for step in self.fake_555.solution:
+            if step.startswith("COMMENT"):
+                pass
+            else:
+                if step.startswith("5"):
+                    step = "7" + step[1:]
+                elif step.startswith("3"):
+                    raise Exception("5x5x5 solution has 3 wide turn")
+                self.rotate(step)
+        
+        self.print_cube_add_comment(desc, tmp_solution_len)
         
     def stage_UD_centers(self):
         self._stage_UD_centers(True)
@@ -3344,6 +3385,9 @@ class RubiksCube777(RubiksCubeNNNOddEdges):
     def myreduce_555_1(self):
         self.mystage_LR_centers()
         self.mystage_UD_centers()
+        self.LR_centers_vertical_bars()
+        self.UD_centers_vertical_bars()
+        self.centers_daisy_solve()
         
     def myreduce_555_2(self):
         self.LR_centers_vertical_bars()
