390f2b161de6a24b14683db38aca89453c9e5d03
i92run <48200564+i92run@users.noreply.github.com>
2024-01-25 01:14:32 +0900

    Update RubiksCube777.py

--- rubikscubennnsolver/RubiksCube777.py
+++ rubikscubennnsolver/RubiksCube777.py
@@ -3422,12 +3422,12 @@ class RubiksCube777(RubiksCubeNNNOddEdges):
     def myreduce_555_6(self):
         self.lt_init()
         # self.LR_centers_vertical_bars()
-        self.UD_centers_vertical_bars()
+        # self.UD_centers_vertical_bars()
         # self.centers_daisy_solve()
 
-        # tmp_solution_len = len(self.solution)
-        # self.lt_step70.solve_via_c()
-        # self.print_cube_add_comment("t-centers solved", tmp_solution_len)
+        tmp_solution_len = len(self.solution)
+        self.lt_step70.solve_via_c()
+        self.print_cube_add_comment("t-centers solved", tmp_solution_len)
 
 def rotate_777(cube, step):
     return [cube[x] for x in swaps_777[step]]
