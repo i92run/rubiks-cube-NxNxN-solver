3e00701d1a5e6b84d22a91f5101d21711cf746d2
i92run <48200564+i92run@users.noreply.github.com>
2024-01-25 00:45:24 +0900

    Update RubiksCube777.py

--- rubikscubennnsolver/RubiksCube777.py
+++ rubikscubennnsolver/RubiksCube777.py
@@ -3399,7 +3399,7 @@ class RubiksCube777(RubiksCubeNNNOddEdges):
     
     def myreduce_555_3(self):
         self.lt_init()
-        self.LR_centers_vertical_bars()
+        # self.LR_centers_vertical_bars()
         self.UD_centers_vertical_bars()
         # self.centers_daisy_solve()
 
